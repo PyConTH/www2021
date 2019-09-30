31a7526c9bd900f7b566a6d9cd2bd1ce825a6024
Jean Jordaan <jean.jordaan@gmail.com>
2019-09-30 14:56:30 +0700

    Tweak Notion export script to only look at subpages

--- get_posts_from_notion.py
+++ get_posts_from_notion.py
@@ -12,47 +12,55 @@ BLOG_HOME = CLIENT.get_block(os.environ["NOTION_BLOG_POSTS_PAGE"])
 
 # Main Loop
 for post in BLOG_HOME.children:
+    if not post.type == 'page':
+        print("Skipping", post)
+        continue
+
     # Handle Frontmatter
     text = """---
 title: %s
 date: "%s"
 description: ""
----""" % (post.title, datetime.datetime.now())
+---
+""" % (post.title, datetime.datetime.now())
     # Handle Title
-    text = text + '\\n\\n' + '# ' + post.title + '\\n\\n'
+    text = text + '\n\n' + '# ' + post.title + '\n\n'
     for content in post.children:
         # Handles H1
         if content.type == 'header':
-            text = text + '# ' + content.title + '\\n\\n'
+            text = text + '# ' + content.title + '\n\n'
         # Handles H2
-        if content.type == 'sub_header':
-            text = text + '## ' + content.title + '\\n\\n'
+        elif content.type == 'sub_header':
+            text = text + '## ' + content.title + '\n\n'
         # Handles H3
-        if content.type == 'sub_sub_header':
-            text = text + '### ' + content.title + '\\n\\n'
+        elif content.type == 'sub_sub_header':
+            text = text + '### ' + content.title + '\n\n'
         # Handles Code Blocks
-        if content.type == 'code':
-            text = text + '```\\n' + content.title + '\\n```\\n'
+        elif content.type == 'code':
+            text = text + '```\n' + content.title + '\n```\n'
         # Handles Images
-        if content.type == 'image':
-            text = text + '![' + content.id + '](' + content.source + ')\\n\\n'
+        elif content.type == 'image':
+            text = text + '![' + content.id + '](' + content.source + ')\n\n'
         # Handles Bullets
-        if content.type == 'bulleted_list':
-            text = text + '* ' + content.title + '\\n'
+        elif content.type == 'bulleted_list':
+            text = text + '* ' + content.title + '\n'
         # Handles Dividers
-        if content.type == 'divider':
-            text = text + '---' + '\\n'
+        elif content.type == 'divider':
+            text = text + '---' + '\n'
         # Handles Basic Text, Links, Single Line Code
-        if content.type == 'text':
-            text = text + content.title + '\\n'
+        elif content.type == 'text':
+            text = text + content.title + '\n'
+        else:
+            print("Ignoring unknown block", content)
 
-    title = post.title.replace(' ', '-')
-    title = title.replace(',', '')
-    title = title.replace(':', '')
-    title = title.replace(';', '')
-    title = title.lower()
 
-    with open('../content/blog/' + title + '.md', 'w') as post:
+        title = post.title.replace(' ', '-')
+        title = title.replace(',', '')
+        title = title.replace(':', '')
+        title = title.replace(';', '')
+        title = title.lower()
+
+    with open('./content/blog/' + title + '.md', 'w') as post:
         post.write(text)
         print('Wrote A New Page')
         print(text)
