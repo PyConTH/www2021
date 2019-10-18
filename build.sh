fbf45f20efb66a7344840e56c18c4f49e872776b
Jean Jordaan <jean.jordaan@gmail.com>
2019-10-18 16:32:51 +0700

    Moved the theme repo to a subtree
    Simplifies Netlify build process

--- build.sh
+++ build.sh
@@ -1,14 +1,4 @@
-# Setup the theme
-if [ -d www2020-elegant ]
-then 
-    cd www2020-elegant 
-    git pull
-    cd ..
-else
-    git clone https://github.com/PyConTH/www2020-elegant.git
-fi
-
-pelican-themes -i www2020-elegant
+pelican-themes -i theme
 
 # Grab the content from Notion
 python get_posts_from_notion.py
