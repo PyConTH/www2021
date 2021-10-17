4f1567b4959a4c882fa423191a21f0d231182632
georgically <40323509+georgically@users.noreply.github.com>
2021-10-18 00:49:55 +0700

    speaker's cards update

--- pelicanconf.py
+++ pelicanconf.py
@@ -396,7 +396,7 @@ JINJA_GLOBALS = {
             "github": "https://github.com/hweecat",
             "twitter": "https://twitter.com/ongchinhwee",
             "linkedin": "https://www.linkedin.com/in/ongchinhwee/",
-            "link": "",
+            "link": "/pages/speakers#Chin-Hwee-Ong",
         },
         {
             "img": "/theme/img/speakers/YevonnaelAndrew.jpg",
@@ -575,11 +575,19 @@ JINJA_GLOBALS = {
             "link": "/pages/speakers#Nat-Weerawan",
         },
 
+         {
             "img": "theme/img/speakers/NunoMuralha.jpg",
             "name": "Nuno Muralha",
             "pronoun": "He/Him",
             "country": "Portugal",
             "linkedin": "https://www.linkedin.com/in/nuno-muralha/",
+            "link": "/pages/speakers#Nuno-Muralha",
+        },
+        {
+            "img": "theme/img/speakers/PraewpanPoriswanish.jpg",
+            "name": "Dr. Praewpan Poriswanish",
+            "pronoun": "She/Her",
+            "country": "Thailand",
             "link": "/pages/speakers#Nuno-Muralha",
         },
     ]
